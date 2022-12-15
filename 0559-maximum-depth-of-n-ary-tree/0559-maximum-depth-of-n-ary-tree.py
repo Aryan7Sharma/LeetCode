877022cd6cbfb79cac3acfa366fa90a495b3d74c
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2022-12-15 09:40:29 +0530

    Time: 51 ms (87.98%), Space: 16.2 MB (16.09%) - LeetHub

--- 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
@@ -10,18 +10,19 @@ class Solution:
         if not root:
             return 0
         
-        res = []
+        res = 0
         
         def dfs(root,level):
+            nonlocal res
             if not root:
                 return 
             
             if root:
                 if not root.children:
-                    res.append(level)
+                    res = max(res,level)
                     
                 for child in root.children:
                     dfs(child, level+1)
                     
         dfs(root,1)
-        return max(res)
+        return res
