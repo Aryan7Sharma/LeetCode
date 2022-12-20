48cea74a3fd8da76a3051cbe15513f5547beee85
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2022-12-20 10:13:40 +0530

    Time: 214 ms (94.71%), Space: 23.1 MB (50.56%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.py
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.py
@@ -6,15 +6,19 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        self.sumNode = 0
-        def inOrder(node):
-            if not node:
-                return None
-            inOrder(node.left)
-            if node.val>=low and node.val<=high:
-                self.sumNode+=node.val
-            inOrder(node.right)
+        # if there is more node return 0 for sum 
+        if not root:
+            return 0
         
-        inOrder(root)
-        return self.sumNode
+        #if root has a value less than low then there is no need to check on left side
+        # because then all node value of the left subtree  must be less than low 
+        if root.val<low:
+            return self.rangeSumBST(root.right, low, high)
+        
+         #if root has a value greater than high then there is no need to check on right side
+        # because then all node value of the right subtree must be greater than high
+        elif root.val>high:
+            return self.rangeSumBST(root.left, low, high)
+        else:
+            return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
         
