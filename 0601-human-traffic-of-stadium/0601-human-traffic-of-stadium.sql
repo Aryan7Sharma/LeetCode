e7c475bd75d8205d3f704ebb144dcee7b2104d4d
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2022-12-21 13:12:23 +0530

    Time: 355 ms (74.16%), Space: 0B (100.00%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+select
+    Distinct t1.*
+From
+    Stadium t1,Stadium t2,Stadium t3
+Where t1.people>=100 and t2.people>=100 and t3.people>=100
+and (
+    (t1.id - t2.id = 1 and t1.id - t3.id = 2 and t2.id - t3.id =1)  -- t1, t2, t3
+    or
+    (t2.id - t1.id = 1 and t2.id - t3.id = 2 and t1.id - t3.id =1) -- t2, t1, t3
+    or
+    (t3.id - t2.id = 1 and t2.id - t1.id =1 and t3.id - t1.id = 2) -- t3, t2, t1
+)
+Order By t1.id;
