19b74a58b577872036f5e9ae0740e1e01c5b3824
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2022-12-28 21:37:58 +0530

    Time: 34 ms (89.07%), Space: 13.9 MB (41.59%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -6,14 +6,13 @@
 #         self.right = right
 class Solution:
     def minDiffInBST(self, root: Optional[TreeNode]) -> int: #DFS
-        self.all_Nvalue = []
-        ans = float('inf')
+        self.prev = -float('inf')
+        self.ans = float('inf')
         def dfs(node):
             if node:
                 dfs(node.left)
-                self.all_Nvalue.append(node.val)
+                self.ans = min(self.ans, node.val - self.prev)
+                self.prev = node.val
                 dfs(node.right)
         dfs(root)
-        for i in range(len(self.all_Nvalue)-1):
-            ans = min(ans, self.all_Nvalue[i+1]-self.all_Nvalue[i])
-        return ans
+        return self.ans
