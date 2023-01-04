da6505f8739779a80bb57f0d0b931bcc4e222383
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2023-01-04 06:07:31 +0530

    Time: 37 ms (72.41%), Space: 13.9 MB (68.83%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -5,9 +5,5 @@ class Solution:
             hashmap[i] = hashmap.get(i, 0) + 1
         for i in s2.split(" "):
             hashmap[i] = hashmap.get(i, 0) + 1
-        res = []
-        for k,v in hashmap.items():
-            if v == 1:
-                res.append(k)
-        return res
+        return [word for word in hashmap if hashmap[word]==1]
         
