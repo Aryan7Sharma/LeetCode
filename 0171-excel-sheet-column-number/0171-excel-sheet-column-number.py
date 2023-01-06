86edabda317f29f9406cfd991e91db0f479d14ad
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2023-01-06 08:13:18 +0530

    Time: 68 ms (22.82%), Space: 13.9 MB (54.88%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -3,9 +3,9 @@ class Solution:
         char = {'A':1,'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
         if len(columnTitle)<2:
             return char[columnTitle[0]]
-        coluNum = char[columnTitle[-1]]
-        coluNum+=(26*char[columnTitle[-2]])
-        columnTitle = columnTitle[::-1]
-        for i in range(2,len(columnTitle)):
-            coluNum+=(26**i)*char[columnTitle[i]]
+        coluNum = char[columnTitle[-1]] + (26*char[columnTitle[-2]])
+        count = 2
+        for i in range(-3,-len(columnTitle)-1,-1):
+            coluNum+=(26**count)*char[columnTitle[i]]
+            count+=1
         return coluNum
