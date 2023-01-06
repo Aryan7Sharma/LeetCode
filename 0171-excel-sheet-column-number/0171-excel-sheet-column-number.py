ead241f084ed8ff01815243a85b415f2eb81cfbc
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2023-01-06 08:16:16 +0530

    Time: 51 ms (52.36%), Space: 14 MB (10.43%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -1,11 +1,10 @@
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         char = {'A':1,'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z':26}
-        if len(columnTitle)<2:
-            return char[columnTitle[0]]
-        coluNum = char[columnTitle[-1]] + (26*char[columnTitle[-2]])
-        count = 2
-        for i in range(-3,-len(columnTitle)-1,-1):
+    
+        coluNum = char[columnTitle[-1]]
+        count = 1
+        for i in range(-2,-len(columnTitle)-1,-1):
             coluNum+=(26**count)*char[columnTitle[i]]
             count+=1
         return coluNum
