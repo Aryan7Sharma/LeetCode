0e5c418add48564caf79e5e166d9e7875be36c2f
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2023-07-25 11:26:14 +0530

    Time: 173 ms (83.75%) | Memory: 18.1 MB (99.03%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowelsHashmap = {"A":0,"E":0,"I":0,"O":0,"U":0,"a":0,"e":0,"i":0,"o":0,"u":0}
+        for i in s:
+            if i in vowelsHashmap:vowelsHashmap[i]+=1
+        #print(vowelsHashmap)
+        sortedVowels = ""
+        for k,v in vowelsHashmap.items():
+            for j in range(v):sortedVowels+=k
+        #print(sortedVowels)
+        sSorted = ""
+        vowelIndex = 0
+        for i in s:
+            if i in vowelsHashmap:
+                sSorted+=sortedVowels[vowelIndex]
+                vowelIndex+=1
+            else:sSorted+=i
+        return sSorted
