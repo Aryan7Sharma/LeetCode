7a12a79cabccaad47f9b7d5e95e92d34b729568a
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2024-11-21 11:43:38 +0530

    Time: 7 ms (51.31%) | Memory: 16.8 MB (24.59%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
@@ -6,6 +6,6 @@ class Solution:
             for num in str(nums[i]):
                 numTotal+=int(num)
             nums[i] = numTotal
-            if(minimum>numTotal):minimum=numTotal
+            minimum = min(minimum, numTotal)
         return minimum
         
