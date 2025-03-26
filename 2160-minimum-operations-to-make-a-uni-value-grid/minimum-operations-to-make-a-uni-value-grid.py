5716e0962104fb708c872adbfa5915223f36a520
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2025-03-26 19:44:58 +0530

    Time: 176 ms (43.86%) | Memory: 39.5 MB (19.30%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -1,19 +1,24 @@
+import statistics
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
         m = len(grid)
         n = len(grid[0])
-        flattened_sorted = sorted([num for row in grid for num in row])
+        flattened_sorted = [num for row in grid for num in row]
         rows = len(grid)
         cols = len(grid[0])
-        sortedGrid = [flattened_sorted[i * cols:(i + 1) * cols] for i in range(rows)]
-        midValue = sortedGrid[m//2][0] if m%2==0  else sortedGrid[m//2][n//2]
+        midValue = self.custom_median(flattened_sorted)
         modulo = midValue%x
         operation = 0
         for r in range(m):
             for c in range(n):
-                currvalue = sortedGrid[r][c]
+                currvalue = grid[r][c]
                 if (currvalue%x)!=modulo:return -1
                 operation+=abs(currvalue-midValue)/x
         return int(operation)
+    def custom_median(self, arr):
+        arr_sorted = sorted(arr)
+        n = len(arr_sorted)
+        mid_index = n // 2
+        return arr_sorted[mid_index]
 
  
