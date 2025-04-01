c8e1a3cbb56bb6d5d2089c92c7366005c48e5a1a
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2025-04-01 16:10:34 +0530

    Time: 6 ms (45.90%) | Memory: 17.9 MB (37.39%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,25 +1,13 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        output = []
-        sDict = {}
-        count = 0 
-        for i in s:
-            if i in sDict:sDict[i]=[sDict[i][0],count]
-            else:sDict[i]=[count,count]
-            count+=1
-        currMax = 0
-        output = []
-        for char,sIndexs in sDict.items():
-            if sIndexs[0]>currMax:
-                output.append(currMax)
-                currMax = sIndexs[1]
-            else:currMax=max(currMax, sIndexs[1])
-        output.append(currMax)
-        sSum = 0
-        for i in range(1,len(output)):
-            sSum+=output[i-1] 
-            output[i]=output[i]-sSum
-        output[0]=output[0]+1
-        return output
-
-
+        last_index = {char: i for i, char in enumerate(s)}  # Store last occurrence of each character
+        partitions = []
+        start, end = 0, 0
+        
+        for i, char in enumerate(s):
+            end = max(end, last_index[char])  # Extend partition to include last occurrence
+            if i == end:  # If current index matches end, finalize partition
+                partitions.append(end - start + 1)
+                start = i + 1  # Start new partition
+        
+        return partitions
