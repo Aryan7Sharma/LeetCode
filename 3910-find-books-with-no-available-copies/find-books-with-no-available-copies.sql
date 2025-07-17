30c47ee8c7c71fd8d27cfb4020297ec5831e0e72
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2025-07-17 12:14:52 +0530

    Time: 266 ms (17.36%) | Memory: 0B (100.00%) - LeetSync

--- 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
+++ 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -1,11 +1,9 @@
--- Write your PostgreSQL query statement below
-Select book_id,title,author,genre,publication_year,current_borrowers From 
-(Select l.book_id,title,author,genre,publication_year,
-count(b.book_id) as  current_borrowers, Max(borrow_date) as latest_borrow_date
+Select l.book_id,title,author,genre,publication_year,
+count(b.book_id) as  current_borrowers
 From borrowing_records b right join library_books l
 on l.book_id=b.book_id AND b.return_date IS NULL
 GROUP BY l.book_id, l.title, l.author, l.genre, l.publication_year, l.total_copies
-having count(b.book_id)=l.total_copies) as sub
+having count(b.book_id)=l.total_copies
 ORDER BY 
-    sub.current_borrowers DESC,
-    sub.title ASC;
+    current_borrowers DESC,
+    title ASC;
