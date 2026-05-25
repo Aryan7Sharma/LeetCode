30e23ebe8768944e096dab0058a121002a623051
Aryan Sharma <103247130+Aryan7Sharma@users.noreply.github.com>
2026-05-25 11:37:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (52.70%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,11 +1,13 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
         size = len(nums)
-        sorted_nums = sorted(nums)
-        
-        for i in range(size):
-            lastele = nums.pop()
-            print(lastele)
-            nums = [lastele] + nums
-            if nums == sorted_nums:return True
-        return False
+        if size<3:return True
+        inversion_count = 0
+
+        for i in range(1,size):
+            if nums[i]<nums[i-1]:
+                inversion_count+=1
+                if inversion_count>1:return False
+        if nums[0]<nums[size-1]:inversion_count+=1
+        print(inversion_count)
+        return inversion_count <= 1
